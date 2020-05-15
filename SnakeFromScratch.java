c5bb474ddf1c27ad6da5ee1891eb0dd5562b12e8
Your Name <you@example.com>
2020-05-15 15:30:51 +0200

    creata la classe principale SnakeFromScratch

---
+++ SnakeFromScratch.java
@@ -0,0 +1,16 @@
+public class SnakeFromScratch{
+	
+	private static void createAndShowGUI()
+	{
+		
+	}
+		
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable(){
+			public void run() {
+				createAndShowGUI();
+			}	
+		});
+	}
+}
+
